5d0e175d61fd3e06263b24db05b528894b8a6884
Tiago Fonseca <tiago@tiagopaiva.me>
2025-02-20 00:07:13 -0300

    adds validation

--- frontend/web/components/modals/CreateFlag.js
+++ frontend/web/components/modals/CreateFlag.js
@@ -573,7 +573,6 @@ const CreateFlag = class extends Component {
 
     const date = moment().toISOString()
 
-    console.log('data', date)
     getChangeRequests(
       getStore(),
       {
@@ -622,9 +621,6 @@ const CreateFlag = class extends Component {
     const noPermissions = this.props.noPermissions
     let regexValid = true
     const metadataEnable = Utils.getPlansPermission('METADATA')
-
-    const { changeRequests, scheduledChangeRequests } = this.state
-    console.log({ changeRequests, out: true, scheduledChangeRequests })
 
     try {
       if (!isEdit && name && regex) {
@@ -967,6 +963,7 @@ const CreateFlag = class extends Component {
                       : 'New Change Request',
                     <ChangeRequestModal
                       showAssignees={is4Eyes}
+                      isScheduledChange={schedule}
                       changeRequest={this.props.changeRequest}
                       onSave={({
                         approvals,
